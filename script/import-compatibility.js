6b556a475c2e395746269c4e80db80bd8ea97d65
nathaniel <nathaniel.simard.42@gmail.com>
2019-05-31 22:00:34 -0400

    Support all imports

--- script/import-compatibility.js
+++ script/import-compatibility.js
@@ -2,18 +2,51 @@ const glob = require('glob')
 const fs = require('fs')
 const ExtensionTag = 'Extension'
 
-const generateExtensionFileImports = imports => {
+const getImport = line => {
+  const regex = /\([^)]+\)/
+  const result = regex.exec(line)
+  return result[0]
+    .replace(/\(/g, '')
+    .replace(/\)/g, '')
+    .replace(/'/g, '')
+}
+
+const getRelativePath = (line, fileName, dots) => {
+  const path = fileName.split('/')
+  path.pop()
+  dots.forEach(r => path.pop())
+  return path
+}
+const generateExtensionFileImport = (line, fileName) => {
+  const dots = line.match(/\.\./g) || []
+  const path = getRelativePath(line, fileName, dots)
+
+  let importLine = getImport(line)
+  dots.forEach(r => (importLine = importLine.replace('../', '')))
+  importLine = importLine.replace('./', '')
+  while (path.length > 1) {
+    importLine = `${path.pop()}/${importLine}`
+  }
+  importLine = `${ExtensionTag}/imports/${importLine}`
+  return line.replace(/require\([^)]+\)/, importLine).replace(/(\/)/g, '.')
+}
+
+const generateExtensionFileImports = (imports, fileName) => {
   return imports
     .filter(i => i.includes("require('."))
     .map(i => {
       return {
-        newImport: i
-          .replace("require('./", `${ExtensionTag}.imports.`)
-          .replace(/\//g, '.')
-          .replace("')", ''),
+        newImport: generateExtensionFileImport(i, fileName),
         oldImport: i
       }
     })
+}
+
+const generateGnomeFileImport = line => {
+  return line
+    .replace("require('", 'imports.')
+    .replace(/\//g, '.')
+    .replace("')", '')
 }
 
 const generateGnomeFileImports = imports => {
@@ -22,10 +55,7 @@ const generateGnomeFileImports = imports => {
     .filter(i => !i.includes(`require('${ExtensionTag}')`))
     .map(i => {
       return {
-        newImport: i
-          .replace("require('", 'imports.')
-          .replace(/\//g, '.')
-          .replace("')", ''),
+        newImport: generateGnomeFileImport(i),
         oldImport: i
       }
     })
@@ -52,7 +82,7 @@ const replaceImportsInFile = fileName => {
   let contents = fs.readFileSync(fileName, 'utf8')
 
   const imports = contents.split('\n').filter(line => line.includes('require'))
-  const extensionImports = generateExtensionFileImports(imports)
+  const extensionImports = generateExtensionFileImports(imports, fileName)
   const gnomeImports = generateGnomeFileImports(imports)
   const extensionsTagImports = generateExtensionTagImports(imports)
 
--- src/bootstrap/windowManager.js
+++ src/bootstrap/windowManager.js
@@ -1,19 +1,11 @@
 const { WindowManager } = require('ui/windowManager')
 const Main = require('ui/main')
-const CustomWindowManager = require('./customWindowManager')
+const { CustomWindowManager } = require('../customWindowManager')
 
 const initialize = (injector, search, settings) => {
-  injector.inject(
-    CustomWindowManager.CustomWindowManager,
-    WindowManager,
-    parent => {
-      return new CustomWindowManager.CustomWindowManager(
-        search,
-        Main.overview,
-        settings
-      )
-    }
-  )
+  injector.inject(CustomWindowManager, WindowManager, parent => {
+    return new CustomWindowManager(search, Main.overview, settings)
+  })
 }
 
 module.exports = { initialize }
